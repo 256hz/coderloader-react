40854114f4657eed2fb37423858d3f10d46a96cc
Abe Dolinger <a.d@256hz.com>
2019-06-03 17:03:27 -0700

    centered githubs

--- src/components/Githubs.js
+++ src/components/Githubs.js
@@ -7,7 +7,7 @@ import {Grid, Card} from 'semantic-ui-react'
 const Githubs = (props) => {
   let githubs = props.githubs.sort( (a,b) => a.order_id - b.order_id )
   return (
-    <Grid columns="equal" divided='vertically'>
+    <Grid columns="equal">
       <SectionHeading text="Featured Repos"
         getContent={ _ => props.getContent(props.githubs)}
         editing={props.editing}
@@ -17,14 +17,20 @@ const Githubs = (props) => {
         startNew={_ => props.startNew('githubs')}
         user={props.user}
       />
-      <Grid.Row>
-        <Card.Group centered>
-          <br />
-          {githubs.map( (github, index) => {
-            return <Github github={github} startEdit={props.startEdit}
-                      loggedIn={props.loggedIn} shiftOrder={props.shiftOrder}/>
-          })}
-        </Card.Group>
+      <Grid.Row columns={16}>
+        <Grid.Column width={2}></Grid.Column>
+        <Grid.Column width={12}>
+          <Card.Group centered>
+            <br />
+            {githubs.map( (github, index) => {
+              return <Github github={github} startEdit={props.startEdit}
+                        loggedIn={props.loggedIn} shiftOrder={props.shiftOrder}
+                        key={github.repo_name + index}
+              />
+            })}
+          </Card.Group>
+        </Grid.Column>
+        <Grid.Column width={2}></Grid.Column>
       </Grid.Row>
     <br />
     </Grid>
