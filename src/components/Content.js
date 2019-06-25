611c1f249650d699435d43ff194f8a75bb1cb6a8
Abe Dolinger <a.d@256hz.com>
2019-06-24 17:36:26 -0700

    fixed props naming issue in Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,30 +8,30 @@ import Githubs from './Githubs'
 import Skills from './Skills'
 import { Grid } from 'semantic-ui-react'
 
-const Content = () => {
+const Content = (props) => {
 
   return (
     <Grid columns="equal">
 
       <Grid.Row key="nav" id="nav">
         <Grid.Column>
-          <Nav openSidebar={this.props.openSidebar} loggedIn={this.props.loggedIn}/>
+          <Nav openSidebar={props.openSidebar} loggedIn={props.loggedIn}/>
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row key="name" id="name">
         <Grid.Column>
           <NamePicIntro 
-            user={this.props.currentUser} editing={this.props.editing}
-            startEdit={this.startEdit}    loggedIn={this.props.loggedIn}/>
+            user={props.currentUser}    editing={props.editing}
+            startEdit={props.startEdit} loggedIn={props.loggedIn}/>
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row key="about" id="about">
         <Grid.Column>
           <AboutMe 
-            user={this.props.currentUser}     editing={this.props.editing}
-            startEdit={this.props.startEdit}  loggedIn={this.props.loggedIn}
+            user={props.currentUser}     editing={props.editing}
+            startEdit={props.startEdit}  loggedIn={props.loggedIn}
           />
         </Grid.Column>
       </Grid.Row>
@@ -39,38 +39,38 @@ const Content = () => {
       <Grid.Row key="skills" id="skills">
         <Grid.Column>
           <Skills 
-            skills={this.props.skills}          editing={this.props.editing}
-            startEdit={this.props.startEdit}    loggedIn={this.props.loggedIn}
-            shiftOrder={this.props.shiftOrder}  startNew={this.props.startNew}
-            user={this.props.currentUser}
+            skills={props.skills}          editing={props.editing}
+            startEdit={props.startEdit}    loggedIn={props.loggedIn}
+            shiftOrder={props.shiftOrder}  startNew={props.startNew}
+            user={props.currentUser}
           />
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row key="jobs" id="jobs">
         <Grid.Column>
-          <Jobs jobs={this.props.jobs} editing={this.props.editing}
-            startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-            shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
-            user={this.props.currentUser}
+          <Jobs jobs={props.jobs} editing={props.editing}
+            startEdit={props.startEdit} loggedIn={props.loggedIn}
+            shiftOrder={props.shiftOrder} startNew={props.startNew}
+            user={props.currentUser}
           />
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row key="github" id="github">
         <Grid.Column>
-          <Githubs githubs={this.props.githubs} editing={this.props.editing}
-            startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-            shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
-            user={this.props.currentUser}
+          <Githubs githubs={props.githubs} editing={props.editing}
+            startEdit={props.startEdit} loggedIn={props.loggedIn}
+            shiftOrder={props.shiftOrder} startNew={props.startNew}
+            user={props.currentUser}
           />
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row key="contact" id="contact">
         <Grid.Column>
-          <Contact user={this.props.currentUser} editing={this.props.editing}
-            startEdit={this.startEdit} loggedIn={this.props.loggedIn}
+          <Contact user={props.currentUser} editing={props.editing}
+            startEdit={startEdit} loggedIn={props.loggedIn}
           />
         </Grid.Column>
       </Grid.Row>
