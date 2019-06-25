9b776e9df0461d27b89be284a049f4a85f0c89a9
Abe Dolinger <a.d@256hz.com>
2019-06-24 17:31:32 -0700

    removed render

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Nav from './Nav'
 import NamePicIntro from './NamePicIntro'
 import AboutMe from './AboutMe'
@@ -10,72 +10,73 @@ import { Grid } from 'semantic-ui-react'
 
 const Content = () => {
 
-  render() {
-    return (
-      <Grid columns="equal">
+  return (
+    <Grid columns="equal">
 
-        <Grid.Row key="nav" id="nav">
-          <Grid.Column>
-            <Nav openSidebar={this.props.openSidebar} loggedIn={this.props.loggedIn}/>
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="nav" id="nav">
+        <Grid.Column>
+          <Nav openSidebar={this.props.openSidebar} loggedIn={this.props.loggedIn}/>
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="name" id="name">
-          <Grid.Column>
-            <NamePicIntro user={this.props.currentUser} editing={this.props.editing}
-              startEdit={this.startEdit} loggedIn={this.props.loggedIn}/>
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="name" id="name">
+        <Grid.Column>
+          <NamePicIntro 
+            user={this.props.currentUser} editing={this.props.editing}
+            startEdit={this.startEdit}    loggedIn={this.props.loggedIn}/>
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="about" id="about">
-          <Grid.Column>
-            <AboutMe user={this.props.currentUser} editing={this.props.editing}
-               startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-            />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="about" id="about">
+        <Grid.Column>
+          <AboutMe 
+            user={this.props.currentUser}     editing={this.props.editing}
+            startEdit={this.props.startEdit}  loggedIn={this.props.loggedIn}
+          />
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="skills" id="skills">
-          <Grid.Column>
-            <Skills skills={this.props.skills} editing={this.props.editing}
-              startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-              shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
-              user={this.props.currentUser}
-            />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="skills" id="skills">
+        <Grid.Column>
+          <Skills 
+            skills={this.props.skills}          editing={this.props.editing}
+            startEdit={this.props.startEdit}    loggedIn={this.props.loggedIn}
+            shiftOrder={this.props.shiftOrder}  startNew={this.props.startNew}
+            user={this.props.currentUser}
+          />
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="jobs" id="jobs">
-          <Grid.Column>
-            <Jobs jobs={this.props.jobs} editing={this.props.editing}
-              startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-              shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
-              user={this.props.currentUser}
-            />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="jobs" id="jobs">
+        <Grid.Column>
+          <Jobs jobs={this.props.jobs} editing={this.props.editing}
+            startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
+            shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
+            user={this.props.currentUser}
+          />
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="github" id="github">
-          <Grid.Column>
-            <Githubs githubs={this.props.githubs} editing={this.props.editing}
-              startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
-              shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
-              user={this.props.currentUser}
-            />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="github" id="github">
+        <Grid.Column>
+          <Githubs githubs={this.props.githubs} editing={this.props.editing}
+            startEdit={this.props.startEdit} loggedIn={this.props.loggedIn}
+            shiftOrder={this.props.shiftOrder} startNew={this.props.startNew}
+            user={this.props.currentUser}
+          />
+        </Grid.Column>
+      </Grid.Row>
 
-        <Grid.Row key="contact" id="contact">
-          <Grid.Column>
-            <Contact user={this.props.currentUser} editing={this.props.editing}
-              startEdit={this.startEdit} loggedIn={this.props.loggedIn}
-            />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Row key="contact" id="contact">
+        <Grid.Column>
+          <Contact user={this.props.currentUser} editing={this.props.editing}
+            startEdit={this.startEdit} loggedIn={this.props.loggedIn}
+          />
+        </Grid.Column>
+      </Grid.Row>
 
-      </Grid>
-    );
-  }
+    </Grid>
+  )
 }
 
-export default Content;
+export default Content
