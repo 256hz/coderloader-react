0a2946e4b7bbae35574ab108489364e8c9324de1
Abe Dolinger <a.d@256hz.com>
2019-06-24 17:23:22 -0700

    Skills - new columnns tactic

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,23 +4,24 @@ import SectionHeading from './SectionHeading'
 
 const Skills = (props) => {
   let skills = props.skills.sort( (a,b) => a.order_id - b.order_id )
-  let columns = 1
-  props.loggedIn ? columns = skills.length*2+3 : columns = skills.length + 2
-  return (
-  <Grid columns='equal'>
-    <SectionHeading text="Primary Skillsets"
-      startEdit={_ => props.startEdit(skills, 'skills')}
-      startNew={_ => props.startNew('skills')}
-      editing={props.editing}
-      loggedIn={props.loggedIn}
-      sectionEdit={true}
-      sectionNew={true}
-      user={props.user}
-    />
-    <Grid.Row columns={columns} verticalAlign="middle" textAlign="center">
-      <Grid.Column>{' '}</Grid.Column>
-      {skills.map( (skill, index) => {
-        return(<div key={skill+index}> 
+  let columns = 0
+  props.loggedIn ? columns = skills.length*2+3 : columns = skills.length * 2 + 1
+  return(
+    <Grid columns='equal'>
+      <SectionHeading text="Primary Skillsets"
+        startEdit={_ => props.startEdit(skills, 'skills')}
+        startNew={_ => props.startNew('skills')}
+        editing={props.editing}
+        loggedIn={props.loggedIn}
+        sectionEdit={true}
+        sectionNew={true}
+        user={props.user}
+      />
+      <Grid.Row columns={columns} verticalAlign="middle" textAlign="center">
+        <Grid.Column>{' '}</Grid.Column>
+        {skills.map( (skill, index) => {
+          return(
+            <div key={skill+index}> 
               {props.loggedIn && index === 0
                 ? <Grid.Column textAlign="center"><Button type="button" onClick={_ => props.shiftOrder('skills', skill, false)} circular icon="long arrow alternate left"/></Grid.Column>
                 : null}
@@ -39,16 +40,17 @@ const Skills = (props) => {
               </Grid.Column>
               {props.loggedIn && index < skills.length - 1
                 ? <Grid.Column textAlign="center"><Button type="button" onClick={_ => props.shiftOrder('skills', skill, true)} circular icon="arrows alternate horizontal"/></Grid.Column>
-                : null}
+                : <Grid.Column>{' '}</Grid.Column>}
               {props.loggedIn && index === skills.length - 1
                 ? <Grid.Column textAlign="center"><Button type="button" onClick={_ => props.shiftOrder('skills', skill, true)} circular icon="long arrow alternate right"/></Grid.Column>
                 : null}
-        </div>)
-      })}
-      <Grid.Column>{' '}</Grid.Column>
-    </Grid.Row>
-    <br />
-  </Grid>
+            </div>
+          )
+        })}
+        <Grid.Column>{' '}</Grid.Column>
+      </Grid.Row>
+      <br />
+    </Grid>
   )
 }
 
