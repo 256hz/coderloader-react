147b4f34e31d5f91b1250893f52347796cf020e2
Abe Dolinger <a.d@256hz.com>
2019-06-25 17:58:07 -0700

    added link parsing to AboutMe & NamePicIntro

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import SectionHeading from './SectionHeading'
 import { Grid } from 'semantic-ui-react'
 
-const AboutMe = (props) => {
+import SectionHeading from './SectionHeading'
 
-  return (
-    <Grid columns='equal'>
+const AboutMe = (props) => {
+  if (!props.user.bio) {
+    return (
+      <Grid columns='equal'>
       <SectionHeading text="About Me"
         startEdit={_ => props.startEdit(props.user, 'users')}
         editing={props.editing}
@@ -13,24 +14,36 @@ const AboutMe = (props) => {
         sectionEdit={true}
       user={props.user}
       />
+      </Grid>
+    )
+  } else {
+    let words = props.user.bio.split(" ")
 
-      <Grid.Row columns={16}>
-        <Grid.Column width={2}></Grid.Column>
-        <Grid.Column width={12}>
-          {props.user.bio}
-        </Grid.Column>
-        <Grid.Column width={2}></Grid.Column>
-      </Grid.Row>
-      <br />
-    </Grid>
-  )
+    return (
+      <Grid columns='equal'>
+        <SectionHeading text="About Me"
+          startEdit={_ => props.startEdit(props.user, 'users')}
+          editing={props.editing}
+          loggedIn={props.loggedIn}
+          sectionEdit={true}
+        user={props.user}
+        />
+
+        <Grid.Row columns={16}>
+          <Grid.Column width={2}></Grid.Column>
+          <Grid.Column width={12}>
+            {words.map( word => {
+              return word.includes('http://')
+                ? <a href={word} target="_blank" rel="noopener noreferrer">{word.slice(7) + " "}</a>
+                : word + " "
+            })}
+          </Grid.Column>
+          <Grid.Column width={2}></Grid.Column>
+        </Grid.Row>
+        <br />
+      </Grid>
+    )
+  }
 }
 
 export default AboutMe
-
-      // <Grid.Row columns={1}>
-      //   <Grid.Column className="heading font-size-large">
-      //     Skills
-      //   </Grid.Column>
-      // </Grid.Row>
-      // <br />
--- src/components/NamePicIntro.js
+++ src/components/NamePicIntro.js
@@ -3,52 +3,46 @@ import { Grid } from 'semantic-ui-react'
 
 
 const NamePicIntro = (props) => {
-   // let title = props.user.title.toUpperCase()
-   return(
-      <Grid columns={16} stretched className="">
-         <Grid.Column width={2}></Grid.Column>
-         <Grid.Column width={6} textAlign="left" verticalAlign="middle">
-            <Grid.Row>
-               <span>Hello, my name is <br /> <br /></span>
-            </Grid.Row>
-            <Grid.Row>
-               <span className="heading-font font-size-huge">
-                  {props.user.first_name} {props.user.last_name} <br />
+   if (!props.user.intro){
+      return(
+         <span className="heading-font font-size-huge">Loading...</span>
+      )
+   } else {
+      let words = props.user.intro.split(" ")
+      return(
+         <Grid columns={16} stretched className="">
+            <Grid.Column width={2}></Grid.Column>
+            <Grid.Column width={6} textAlign="left" verticalAlign="middle">
+               <Grid.Row>
+                  <span>Hello, my name is <br /> <br /></span>
+               </Grid.Row>
+               <Grid.Row>
+                  <span className="heading-font font-size-huge">
+                     {props.user.first_name} {props.user.last_name} <br />
+                  </span>
+               </Grid.Row>
+               <Grid.Row>
+               <span className="emphasis">{props.user.title}</span>
+               </Grid.Row>
+               <Grid.Row>
+               <span>
+                  {words.map( word => {
+                      return word.includes('http://')
+                        ? <a href={word} target="_blank" rel="noopener noreferrer">{word.slice(7) + " "}</a>
+                        : word + " "
+                  })}
                </span>
-            </Grid.Row>
-            <Grid.Row>
-            <span>{props.user.title}</span>
-            </Grid.Row>
-            <Grid.Row>
-            <span>{props.user.intro}</span>
-            </Grid.Row>
-         </Grid.Column>
+               </Grid.Row>
+            </Grid.Column>
 
-         <Grid.Column width={6} textAlign="left">
-            <img className="image-circle-portrait"
-               src={props.user.img_url} alt="portrait"></img>
-         </Grid.Column>
-         <Grid.Column width={2}></Grid.Column>
-      </Grid>
-   )
+            <Grid.Column width={6} textAlign="left">
+               <img className="image-circle-portrait"
+                  src={props.user.img_url} alt="portrait"></img>
+            </Grid.Column>
+            <Grid.Column width={2}></Grid.Column>
+         </Grid>
+      )
+   }
 }
 
 export default NamePicIntro
-
-// <div className="ui grid gray-bg">
-// <div className="sixteen wide column row">
-//    <div className="eight wide column middle aligned">
-//    
-//          Hello, my name is <br />
-//          {props.user.first_name} {props.user.last_name}
-//       
-//          {props.user.intro}
-// 
-//    </div>         
-//    <div className='eight wide column middle aligned'>
-//       <img className="image-circle"
-//             src={props.user.img_url}
-//             alt="portrait"></img>
-//    </div>            
-// </div>
-// </div>
